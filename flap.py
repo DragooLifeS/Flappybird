652c2f7df540a6aee074af776406554a1031a36d
kotreg <dobushinskiy@mail.ru>
2021-05-11 17:49:45 +0300

    New

--- flap.py
+++ flap.py
@@ -1,7 +1,10 @@
 import pygame
 import random
 
-class Bird:
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(self)
+          
     
    # def tube(self):
    # нужно сделать 2 трубы сверху и снизу и по логике через рандом указывать высоту промежутка где будет птичка лететь
@@ -14,15 +17,15 @@ class Bird:
         self.game = game
 
 
-    def key(self, x, y):
+#    def key(self, x, y):
+#        
+#        keys = pygame.key.get_pressed()
+#
+#      if keys(pygame.K_ESC): #потом тут будет еще "and "died" died = будет команда когда птичка умирает или чтото такое
+#         open(menu1)
+#    
+#      elif keys(pygame.K_ENTER):
+#         open(main)
         
-        keys = pygame.key.get_pressed()
-
-        if keys(pygame.K_ESC): #потом тут будет еще "and "died" died = будет команда когда птичка умирает или чтото такое
-            open(menu1)
-        
-        elif keys(pygame.K_ENTER):
-            open(main)
-        
-        #elif keys(pygame.K_SPACE):
-        #ТУТ БУДЕТ ФИЗИКА СРАНАЯ
+        elif keys(pygame.K_SPACE):
+        нужны спрайты
--- main.py
+++ main.py
@@ -2,13 +2,41 @@ import pygame
 from flap import Bird
 pygame.init()
 
+win_size = 700
 win = pygame.display.set_mode((500,500))
 bc = (255,255,255)
+menu_color = (40,40,40)
+
+font0 = pygame.font.SysFont("arial", 64)
+red = (255,0,0)
+
+def draw_text(text, font, color, frame, x, y):
+    textobj = font.render(text,1,color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x,y)
+    frame.blit(textobj, textrect)
+
+def menu(win):
+    running = True
+    while running:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        win.fill(menu_color)
+        draw_text("Menu", font0, red,)
+        pygame.display.update()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                menu(win)
 
     win.fill(bc)
 
@@ -19,3 +47,4 @@ while True:
     pygame.display.update()
 
     pygame.time.delay(10)
+
